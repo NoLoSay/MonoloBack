f0dc334c86e960b6c4809e17f0e70ec57caab622
aurelenc <aurele.nicolas@epitech.eu>
2023-11-09 16:51:15 +0900

    Add swagger to video api

--- apps/video/src/main.ts
+++ apps/video/src/main.ts
@@ -7,12 +7,24 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const globalPrefix = '';
+  // app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3002;
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NoLoVideo')
+    .setDescription('A NoLoSay API')
+    .setVersion(process.env['API_VERSION'])
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
