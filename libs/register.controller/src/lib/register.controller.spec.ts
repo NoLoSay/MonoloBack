577d405140e951bf8e31283e984d8db63960e9ef
Ugo Boulestreau <72016899+UgoBoulestreau@users.noreply.github.com>
2023-12-29 19:37:34 +0900

    register controller unit test

--- libs/register.controller/src/lib/register.controller.spec.ts
+++ libs/register.controller/src/lib/register.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RegisterController } from './register.controller';
+import { CreateUserDto, UsersService } from '@noloback/users.service';
 
-describe('RegisterController', () => {
+jest.mock('@noloback/users.service');
+describe('UsersController', () => {
   let controller: RegisterController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RegisterController],
+      providers: [UsersService],
     }).compile();
 
     controller = module.get<RegisterController>(RegisterController);
+    service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('register', () => {
+    it('should register a user and return the result', async () => {
+      // Arrange
+      const mockUserRegister: CreateUserDto = {
+        username: 'testuser',
+        password: 'testpassword',
+        email: "testuser@email.com"
+      };
+      const createdUser = {
+        id: 1,
+        username: mockUserRegister.username,
+        email: mockUserRegister.email
+      };
+
+      jest.spyOn(service, 'create').mockResolvedValue(createdUser);
+
+      const result = await controller.register(mockUserRegister);
+
+      expect(result).toEqual(createdUser);
+      expect(service.create).toHaveBeenCalledWith(mockUserRegister);
+    });
   });
 });
