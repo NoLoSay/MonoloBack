d95962d1c3a6a51f9e5eafe00e8e76502b817902
aurelenc <aurele.nicolas@epitech.eu>
2024-02-06 00:05:11 +0900

    Add essencial components

--- shared/index.ts
+++ shared/index.ts
@@ -1,3 +1,6 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
 export * from './components/ui/button';
+export * from './components/ui/dropdown-menu';
+
+export * from './lib/utils';
