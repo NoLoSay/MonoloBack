7681584360dc98d2cce5f56315570ebf6f0d0304
Quentin Camilleri <q.camilleri.pro@gmail.com>
2024-02-17 16:14:49 +0200

    Fix wrong importation of ItemService

--- libs/items.controller/src/lib/items.controller.module.ts
+++ libs/items.controller/src/lib/items.controller.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { AuthServiceModule } from '@noloback/auth.service'
-import { ItemsServiceModule } from 'libs/items.service/src'
+import { ItemsServiceModule } from '@noloback/items.service'
 import { ItemsController } from './items.controller'
 import { LocationsReferentsServiceModule } from '@noloback/locations.referents.service'
 import { VideoServiceModule } from '@noloback/video.service'
