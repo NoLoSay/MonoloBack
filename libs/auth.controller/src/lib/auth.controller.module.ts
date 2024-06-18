50d759cc4760805d9dafea09dcaa5f4b893e94b5
Ugo Boulestreau <72016899+UgoBoulestreau@users.noreply.github.com>
2024-06-18 20:08:28 +0900

    fix compoilation error and module importation

--- libs/auth.controller/src/lib/auth.controller.module.ts
+++ libs/auth.controller/src/lib/auth.controller.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthServiceModule } from '@noloback/auth.service';
+import { MailConfirmationModule } from '@noloback/mail-confirmation.service';
 
 @Module({
   controllers: [AuthController],
   providers: [],
   exports: [],
-  imports: [AuthServiceModule],
+  imports: [
+    AuthServiceModule,
+    MailConfirmationModule
+  ],
 })
 export class AuthControllerModule {}
