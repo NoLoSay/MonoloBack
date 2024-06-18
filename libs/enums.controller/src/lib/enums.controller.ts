51db51f0b886f17358d17a511bc78c0e2fc2f5fb
Quentin Camilleri <q.camilleri.pro@gmail.com>
2024-06-18 11:04:20 +0200

    Add Enum Admin List and Patch routes

--- libs/enums.controller/src/lib/enums.controller.ts
+++ libs/enums.controller/src/lib/enums.controller.ts
@@ -1,6 +1,7 @@
 import {
   Controller,
   Get,
+  Patch,
   Post,
   Body,
   Put,
@@ -9,37 +10,167 @@ import {
   ParseIntPipe,
   Request
 } from '@nestjs/common'
+import {
+  PersonType,
+  Role,
+  SiteTag,
+  SiteType,
+  ValidationStatus
+} from '@prisma/client/base'
 import { EnumsService } from '@noloback/enums.service'
 import { ADMIN, Roles } from '@noloback/roles'
-import { PersonType, Role, SiteTag, SiteType, ValidationStatus } from '@prisma/client/base'
+import { LoggerService } from '@noloback/logger-lib'
 
 @Controller('enums')
 export class EnumsController {
   constructor (private readonly enumsService: EnumsService) {}
 
   @Get('roles')
-  async getRoles (): Promise<{role:Role, color: string}[]> {
-    return this.enumsService.getRolesColors()
+  async getRoles (): Promise<{ role: Role; color: string }[]> {
+    return await this.enumsService.getRolesColors()
+  }
+
+  @Roles([ADMIN])
+  @Get('roles/admin')
+  async getAdminRoles (): Promise<{ id: number; role: Role; color: string }[]> {
+    return await this.enumsService.getAdminRolesColors()
+  }
+
+  @Roles([ADMIN])
+  @Patch('roles/:id')
+  async updateRoles (
+    @Request() request: any,
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<{ id: number; role: Role; color: string }> {
+    LoggerService.sensitiveLog(
+      +request.user.activeProfile.id,
+      'UPDATE',
+      'Role Colors',
+      +id,
+      JSON.stringify(request.body)
+    )
+    return await this.enumsService.roleColorsPatch(id, request.body)
   }
 
   @Get('validation-statuses')
-  async getValidationStatuses (): Promise<{validationStatus:ValidationStatus, color: string}[]> {
-    return this.enumsService.getValidationStatusesColors()
+  async getValidationStatuses (): Promise<
+    { validationStatus: ValidationStatus; color: string }[]
+  > {
+    return await this.enumsService.getValidationStatusesColors()
+  }
+
+  @Roles([ADMIN])
+  @Get('validation-statuses/admin')
+  async getAdminValidationStatuses (): Promise<
+    { id: number; validationStatus: ValidationStatus; color: string }[]
+  > {
+    return await this.enumsService.getAdminValidationStatusesColors()
+  }
+
+  @Roles([ADMIN])
+  @Patch('validation-statuses/:id')
+  async updateValidationStatuses (
+    @Request() request: any,
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<{
+    id: number
+    validationStatus: ValidationStatus
+    color: string
+  }> {
+    LoggerService.sensitiveLog(
+      +request.user.activeProfile.id,
+      'UPDATE',
+      'Validation Status Colors',
+      +id,
+      JSON.stringify(request.body)
+    )
+    return await this.enumsService.validationStatusColorsPatch(id, request.body)
   }
 
   @Get('person-types')
-  async getPersonTypes (): Promise<{personType:PersonType, color: string}[]> {
-    return this.enumsService.getPersonTypesColors()
+  async getPersonTypes (): Promise<{ personType: PersonType; color: string }[]> {
+    return await this.enumsService.getPersonTypesColors()
+  }
+
+  @Roles([ADMIN])
+  @Get('person-types/admin')
+  async getAdminPersonTypes (): Promise<
+    { id: number; personType: PersonType; color: string }[]
+  > {
+    return await this.enumsService.getAdminPersonTypesColors()
+  }
+
+  @Roles([ADMIN])
+  @Patch('person-types/:id')
+  async updatePersonTypes (
+    @Request() request: any,
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<{ id: number; personType: PersonType; color: string }> {
+    LoggerService.sensitiveLog(
+      +request.user.activeProfile.id,
+      'UPDATE',
+      'Person Type Colors',
+      +id,
+      JSON.stringify(request.body)
+    )
+    return await this.enumsService.personTypeColorsPatch(id, request.body)
   }
 
   @Get('site-types')
-  async getSiteTypes (): Promise<{siteType:SiteType, color: string}[]> {
-    return this.enumsService.getSiteTypesColors()
+  async getSiteTypes (): Promise<{ siteType: SiteType; color: string }[]> {
+    return await this.enumsService.getSiteTypesColors()
   }
 
+  @Roles([ADMIN])
+  @Get('site-types/admin')
+  async getAdminSiteTypes (): Promise<
+    { id: number; siteType: SiteType; color: string }[]
+  > {
+    return await this.enumsService.getAdminSiteTypesColors()
+  }
+
+  @Roles([ADMIN])
+  @Patch('site-types/:id')
+  async updateSiteTypes (
+    @Request() request: any,
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<{ id: number; siteType: SiteType; color: string }> {
+    LoggerService.sensitiveLog(
+      +request.user.activeProfile.id,
+      'UPDATE',
+      'Site Type Colors',
+      +id,
+      JSON.stringify(request.body)
+    )
+    return await this.enumsService.siteTypeColorsPatch(id, request.body)
+  }
 
   @Get('site-tags')
-  async getSiteTags (): Promise<{siteTag:SiteTag, color: string}[]> {
-    return this.enumsService.getSiteTagsColors()
+  async getSiteTags (): Promise<{ siteTag: SiteTag; color: string }[]> {
+    return await this.enumsService.getSiteTagsColors()
+  }
+
+  @Roles([ADMIN])
+  @Get('site-tags/admin')
+  async getAdminSiteTags (): Promise<
+    { id: number; siteTag: SiteTag; color: string }[]
+  > {
+    return await this.enumsService.getAdminSiteTagsColors()
+  }
+
+  @Roles([ADMIN])
+  @Patch('site-tags/:id')
+  async updateSiteTags (
+    @Request() request: any,
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<{ id: number; siteTag: SiteTag; color: string }> {
+    LoggerService.sensitiveLog(
+      +request.user.activeProfile.id,
+      'UPDATE',
+      'Site Tag Colors',
+      +id,
+      JSON.stringify(request.body)
+    )
+    return await this.enumsService.siteTagColorsPatch(id, request.body)
   }
 }
--- libs/enums.service/src/lib/enums.service.ts
+++ libs/enums.service/src/lib/enums.service.ts
@@ -7,7 +7,11 @@ import {
   SiteType,
   SiteTag
 } from '@noloback/prisma-client-base'
-import { Injectable } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common'
 
 @Injectable()
 export class EnumsService {
@@ -15,53 +19,180 @@ export class EnumsService {
     private prismaBase: PrismaBaseService //private loggingService: LoggerService
   ) {}
 
+  isColor (color: string): boolean {
+    return /^#[0-9A-F]{6}$/i.test(color)
+  }
+
   async getRoles (): Promise<Role[]> {
     return Object.values(Role)
   }
 
-  async getRolesColors (): Promise<{role: Role, color: string}[]> {
+  async getRolesColors (): Promise<{ role: Role; color: string }[]> {
     return this.prismaBase.roleColor.findMany({
       select: { role: true, color: true }
     })
+  }
+
+  async getAdminRolesColors (): Promise<
+    { id: number; role: Role; color: string }[]
+  > {
+    return this.prismaBase.roleColor.findMany({})
+  }
+
+  async roleColorsPatch (
+    id: number,
+    body: any
+  ): Promise<{ id: number; role: Role; color: string }> {
+    if (body.color && !this.isColor(body.color)) {
+      throw new BadRequestException('Color not valid')
+    }
+    try {
+      return await this.prismaBase.roleColor.update({
+        where: { id },
+        data: body
+      })
+    } catch {
+      throw new NotFoundException('Enum not found')
+    }
   }
 
   async getValidationStatuses (): Promise<ValidationStatus[]> {
     return Object.values(ValidationStatus)
   }
 
-  async getValidationStatusesColors (): Promise<{validationStatus: ValidationStatus, color: string}[]> {
+  async getValidationStatusesColors (): Promise<
+    { validationStatus: ValidationStatus; color: string }[]
+  > {
     return this.prismaBase.validationStatusColor.findMany({
       select: { validationStatus: true, color: true }
     })
+  }
+
+  async getAdminValidationStatusesColors (): Promise<
+    { id: number; validationStatus: ValidationStatus; color: string }[]
+  > {
+    return this.prismaBase.validationStatusColor.findMany({})
+  }
+
+  async validationStatusColorsPatch (
+    id: number,
+    body: any
+  ): Promise<{
+    id: number
+    validationStatus: ValidationStatus
+    color: string
+  }> {
+    if (body.color && !this.isColor(body.color)) {
+      throw new BadRequestException('Color not valid')
+    }
+    try {
+      return await this.prismaBase.validationStatusColor.update({
+        where: { id },
+        data: body
+      })
+    } catch {
+      throw new NotFoundException('Enum not found')
+    }
   }
 
   async getPersonTypes (): Promise<PersonType[]> {
     return Object.values(PersonType)
   }
 
-  async getPersonTypesColors (): Promise<{personType: PersonType, color: string}[]> {
+  async getPersonTypesColors (): Promise<
+    { personType: PersonType; color: string }[]
+  > {
     return this.prismaBase.personTypeColor.findMany({
       select: { personType: true, color: true }
     })
+  }
+
+  async getAdminPersonTypesColors (): Promise<
+    { id: number; personType: PersonType; color: string }[]
+  > {
+    return this.prismaBase.personTypeColor.findMany({})
+  }
+
+  async personTypeColorsPatch (
+    id: number,
+    body: any
+  ): Promise<{ id: number; personType: PersonType; color: string }> {
+    if (body.color && !this.isColor(body.color)) {
+      throw new BadRequestException('Color not valid')
+    }
+    try {
+      return await this.prismaBase.personTypeColor.update({
+        where: { id },
+        data: body
+      })
+    } catch {
+      throw new NotFoundException('Enum not found')
+    }
   }
 
   async getSiteTypes (): Promise<SiteType[]> {
     return Object.values(SiteType)
   }
 
-  async getSiteTypesColors (): Promise<{siteType: SiteType, color: string}[]> {
+  async getSiteTypesColors (): Promise<{ siteType: SiteType; color: string }[]> {
     return this.prismaBase.siteTypeColor.findMany({
       select: { siteType: true, color: true }
     })
+  }
+
+  async getAdminSiteTypesColors (): Promise<
+    { id: number; siteType: SiteType; color: string }[]
+  > {
+    return this.prismaBase.siteTypeColor.findMany({})
+  }
+
+  async siteTypeColorsPatch (
+    id: number,
+    body: any
+  ): Promise<{ id: number; siteType: SiteType; color: string }> {
+    if (body.color && !this.isColor(body.color)) {
+      throw new BadRequestException('Color not valid')
+    }
+    try {
+      return await this.prismaBase.siteTypeColor.update({
+        where: { id },
+        data: body
+      })
+    } catch {
+      throw new NotFoundException('Enum not found')
+    }
   }
 
   async getSiteTags (): Promise<SiteTag[]> {
     return Object.values(SiteTag)
   }
 
-  async getSiteTagsColors (): Promise<{siteTag: SiteTag, color: string}[]> {
+  async getSiteTagsColors (): Promise<{ siteTag: SiteTag; color: string }[]> {
     return this.prismaBase.siteTagColor.findMany({
       select: { siteTag: true, color: true }
     })
+  }
+
+  async getAdminSiteTagsColors (): Promise<
+    { id: number; siteTag: SiteTag; color: string }[]
+  > {
+    return this.prismaBase.siteTagColor.findMany({})
+  }
+
+  async siteTagColorsPatch (
+    id: number,
+    body: any
+  ): Promise<{ id: number; siteTag: SiteTag; color: string }> {
+    if (body.color && !this.isColor(body.color)) {
+      throw new BadRequestException('Color not valid')
+    }
+    try {
+      return await this.prismaBase.siteTagColor.update({
+        where: { id },
+        data: body
+      })
+    } catch {
+      throw new NotFoundException('Enum not found')
+    }
   }
 }
