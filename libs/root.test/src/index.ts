8a189daace8888e37b481729ad8c68299c229a82
aurelenc <aurele.nicolas@epitech.eu>
2024-06-19 23:22:55 +0900

    Add root test module

---
+++ libs/root.test/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/root.test.module';
+// export * from './lib/video.service.mock'
---
+++ libs/root.test/src/lib/root.test.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+// import { VideoController } from '@noloback/video.controller';
+// import { VideoService } from '@noloback/video.service.mock';
+
+@Module({
+  controllers: [],
+  providers: [],
+})
+export class RootTestModule {}
