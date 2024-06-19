6268d42dd7571ddbe6048527c1eebb121aa32ae1
aurelenc <aurele.nicolas@epitech.eu>
2024-06-19 21:39:33 +0900

    Use new custom date formatter

--- libs/persons.service/src/lib/persons.service.module.ts
+++ libs/persons.service/src/lib/persons.service.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { LoggerLibModule } from '@noloback/logger-lib';
 import { PrismaClientBaseModule } from '@noloback/prisma-client-base';
 import { PersonsService } from './persons.service';
+import { UtilsServiceModule } from '@noloback/utils.service';
 
 @Module({
   controllers: [],
   providers: [PersonsService],
   exports: [PersonsService],
-  imports: [PrismaClientBaseModule, LoggerLibModule],
+  imports: [PrismaClientBaseModule, LoggerLibModule, UtilsServiceModule],
 })
 export class PersonsServiceModule {}
