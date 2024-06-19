a58e65c2ada3f98cf1b3dc5a593267dc5c097d1a
aurelenc <aurele.nicolas@epitech.eu>
2024-06-19 23:23:10 +0900

    Mock video service demo

--- libs/video.service/src/lib/video.service.spec.ts
+++ libs/video.service/src/lib/video.service.spec.ts
@@ -1,21 +1,73 @@
 import { HttpModule } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import { VideoService } from './video.service';
+import { PrismaBaseService } from '@noloback/prisma-client-base';
+import { VideoCommonReturn } from '@noloback/api.returns';
+import { VideoCommonDbReturn } from '@noloback/db.returns';
+import { LoggerService } from '@noloback/logger-lib';
+import { ProfileService } from '@noloback/profile.service';
+import { SitesManagersService } from '@noloback/sites.managers.service';
+
+class Provider {
+  constructor(url: string) {
+    this.url = url
+  }
+
+  url: string
+}
+
+class HostingProvider {
+  async findUnique(): Promise<Provider> {
+    return new Provider("aaa")
+  }
+}
+
+class MockPrisma {
+  constructor() {
+    this.hostingProvider = new HostingProvider();
+  }
+
+  hostingProvider: HostingProvider;
+}
+
+class EmptyService {
+
+}
 
 describe('videoservice', () => {
   let app: TestingModule;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
-      providers: [VideoService],
+      providers: [VideoService,
+        {
+          provide: PrismaBaseService,
+          useClass: MockPrisma
+        },
+        {
+          provide: LoggerService,
+          useClass: EmptyService
+        },
+        {
+          provide: ProfileService,
+          useClass: EmptyService
+        },
+        {
+          provide: SitesManagersService,
+          useClass: EmptyService
+        }
+      ],
       imports: [HttpModule],
     }).compile();
   });
 
   describe('getYoutube', () => {
-    it('should return 1234', async () => {
+    it('should return the mocked video', async () => {
       const service = app.get(VideoService);
-      expect(await service.getYoutube('1234')).toBe('1234');
+      const video: VideoCommonReturn = new VideoCommonReturn(new VideoCommonDbReturn);
+      expect(await service.getYoutube(video)).toStrictEqual({video: video, url: "aaa"
+        .replace('${videoUUID}', video.uuid)
+        .replace('${providerVideoId}', video.hostingProviderVideoId)});
     });
   });
 });
