6957ddc27162e8b47758f97e7f39d9d5af9d72a8
aurelenc <aurele.nicolas@epitech.eu>
2024-07-07 22:09:24 +0900

    Add picture controller on 3002 api to stream pictures

--- libs/pictures.controller/src/lib/pictures.controller.ts
+++ libs/pictures.controller/src/lib/pictures.controller.ts
@@ -1,25 +1,58 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Body,
-    Put,
-    Param,
-    Delete,
-    ParseIntPipe,
-    Request,
-    Response,
-    UnauthorizedException,
-    Patch,
-    UseInterceptors,
-    UploadedFile
-} from '@nestjs/common'
+  Controller,
+  Get,
+  Param,
+  Res,
+  NotFoundException,
+} from '@nestjs/common';
 import { PicturesService } from '@noloback/pictures.service';
+import { Public } from '@noloback/jwt';
+import { Response } from 'express';
+import * as path from 'path';
 
 @Controller('pictures')
 export class PicturesController {
-  constructor (private readonly picturesService: PicturesService) {}
+  constructor(private readonly picturesService: PicturesService) {}
 
-//   @Get()
-//   async findAll () {}
+  @Get(':uuid')
+  @Public()
+  async findOne(
+    @Param('uuid') uuid: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const pictureBuffer = await this.picturesService.getPicture(uuid);
+      
+      const picture = await this.picturesService.getPictureDetails(uuid);
+      const extension = path.extname(picture.localPath).toLowerCase();
+      let contentType = 'application/octet-stream';
+
+      switch (extension) {
+        case '.jpg':
+        case '.jpeg':
+          contentType = 'image/jpeg';
+          break;
+        case '.png':
+          contentType = 'image/png';
+          break;
+        case '.gif':
+          contentType = 'image/gif';
+          break;
+      }
+
+      res.set({
+        'Content-Type': contentType,
+        'Content-Length': pictureBuffer.length,
+      });
+
+      res.send(pictureBuffer);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        res.status(404).send('Picture not found');
+      } else {
+        console.error(`Error serving picture: ${error.message}`);
+        res.status(500).send('Internal Server Error');
+      }
+    }
+  }
 }
--- libs/pictures.service/src/lib/pictures.service.ts
+++ libs/pictures.service/src/lib/pictures.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import {
   Picture,
   PrismaBaseService,
 } from '@noloback/prisma-client-base'
-import { randomUUID } from "crypto";
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class PicturesService {
@@ -16,13 +16,52 @@ export class PicturesService {
       const picture = (await this.prismaBase.picture.create({
         data: {
           localPath: picturePath,
-          hostingUrl: `${process.env["API_URL"]}:${process.env["VIDEO_API_PORT"]}/pictures/${randomUUID()}`
+          hostingUrl: ``
         }
       }))
-      return picture;
+
+      const updatedPicture = await this.prismaBase.picture.update({
+        where: {
+          uuid: picture.uuid
+        },
+        data: {
+          hostingUrl: `${process.env["API_URL"]}:${process.env["VIDEO_API_PORT"]}/pictures/${picture.uuid}`
+        }
+      });
+
+      return updatedPicture;
     } catch (error: any) {
       console.error(`Error creating picture: ${error.message}`);
       throw new InternalServerErrorException(`Error creating picture`);
     }
+  }
+
+  async getPicture(uuid: string) {
+    const picture = await this.prismaBase.picture.findUnique({
+      where: {
+        uuid: uuid
+      }
+    });
+    
+    if (!picture) {
+      throw new NotFoundException(`Picture with UUID ${uuid} not found`);
+    }
+
+    const fileContent = readFileSync(picture.localPath);
+    return fileContent;
+  }
+
+  async getPictureDetails(uuid: string): Promise<Picture> {
+    const picture = await this.prismaBase.picture.findUnique({
+      where: {
+        uuid: uuid
+      }
+    });
+  
+    if (!picture) {
+      throw new NotFoundException(`Picture with UUID ${uuid} not found`);
+    }
+  
+    return picture;
   }
 }
