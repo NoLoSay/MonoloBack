4255de2d8b79edae792c574b33df32fd843157b5
aurelenc <aurele.nicolas@epitech.eu>
2024-08-18 17:22:24 +0200

    Add sanctions service

---
+++ libs/sanctions.service/src/lib/sanctions.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaBaseService } from '@noloback/prisma-client-base';
+
+@Injectable()
+export class SanctionsService {
+  constructor (
+    private prismaBase: PrismaBaseService,
+  ) {}
+
+  async count() {
+    
+  }
+
+  async findAll() {
+    
+  }
+
+  async findOne() {
+    
+  }
+
+  async create() {
+    
+  }
+
+  async update() {
+    
+  }
+
+  async patch() {
+    
+  }
+
+  async delete() {
+    
+  }
+}
