11f88411f943d0a46eacc88510030ab3f5c9b40f
Aurèle Nicolas <aurele.nicolas@epitech.eu>
2024-09-16 17:44:49 +0200

    Fix test build errors

--- libs/root.test/src/lib/root.test.module.ts
+++ libs/root.test/src/lib/root.test.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
-// import { VideoController } from '@noloback/video.controller';
-// import { VideoService } from '@noloback/video.service.mock';
+import { LoggerService } from '@noloback/logger-lib';
+import { PrismaBaseService, PrismaClientBaseModule } from '@noloback/prisma-client-base';
+import { ProfileService } from '@noloback/profile.service';
+import { SitesManagersService } from '@noloback/sites.managers.service';
+import { VideoService } from '@noloback/video.service';
 
 @Module({
-  controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: VideoService,
+      useValue: {},
+    },
+    {
+      provide: PrismaBaseService,
+      useValue: {},
+    },
+    {
+      provide: LoggerService,
+      useValue: {},
+    },
+    {
+      provide: ProfileService,
+      useValue: {},
+    },
+    {
+      provide: SitesManagersService,
+      useValue: {},
+    },
+  ],
+  exports: [PrismaBaseService, LoggerService, ProfileService, SitesManagersService, VideoService],
 })
 export class RootTestModule {}
