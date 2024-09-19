25829091a45efc8f3d891172eed5e92a0bacc6cb
Aurèle Nicolas <aurele.nicolas@epitech.eu>
2024-09-19 19:25:21 +0200

    start testing user service

--- libs/users.service/src/lib/users.service.spec.ts
+++ libs/users.service/src/lib/users.service.spec.ts
@@ -1,14 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { RootTestModule } from '@noloback/root.test';
+import { UserCreateModel } from '@noloback/api.request.bodies';
+import { ConflictException } from '@nestjs/common';
+import { LoggerService } from '@noloback/logger-lib';
+import { PrismaBaseService } from '@noloback/prisma-client-base';
 
 describe('UsersService', () => {
   let service: UsersService;
+  // let fakePrismaUser: UserCreateModel[] = [];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-      imports: [RootTestModule],
+      providers: [UsersService, {
+        provide: LoggerService,
+        useValue: {
+          log: jest.fn(),
+        },
+      },
+      {
+        provide: PrismaBaseService,
+        useValue: {
+          // user: {
+          //   create: jest.fn((userCreate: UserCreateModel) : any => {
+          //     if (fakePrismaUser.find(user => user.email === userCreate.email)) {
+          //       throw new ConflictException();
+          //     }
+          //     fakePrismaUser.push(userCreate);
+          //   }),
+          //   findUnique: jest.fn(),
+          // },
+        },
+      }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -17,4 +40,24 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // it('should create user', async () => {
+  //   const createUserDto: UserCreateModel = {
+  //     username: 'test',
+  //     email: 'test',
+  //     password: 'test',
+  //   };
+  //   const user = await service.create(createUserDto);
+  //   expect(user).toBeDefined();
+  // });
+
+  // it('should throw on duplicate user', async () => {
+  //   const createUserDto: UserCreateModel = {
+  //     username: 'test',
+  //     email: 'test',
+  //     password: 'test',
+  //   };
+  //   await service.create(createUserDto);
+  //   await expect(service.create(createUserDto)).rejects.toThrowError(ConflictException);
+  // });
 });
