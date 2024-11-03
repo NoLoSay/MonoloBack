1adf88f5b292b8feb1a904d206d48aa86272ca03
Aurèle Nicolas <aurele.nicolas@epitech.eu>
2024-11-03 18:01:48 +0100

    fix - invalid char in ts exports

--- models/index.ts
+++ models/index.ts
@@ -3,9 +3,9 @@ export * from './filters-get-many';
 
 export * as swagger from './swagger/index';
 export * as guards from './guards/index';
-export * as requests-constructor from './requests.constructor/index';
-export * as db-calls from './db.calls/index';
-export * as db-returns from './db.returns/index';
-export * as api-request-bodies from './api.request.bodies/index';
-export * as api-querry-parameters from './api.querry.parameters/index';
-export * as api-returns from './api.returns/index';
+export * as 'requests-constructor' from './requests.constructor/index';
+export * as 'db-calls' from './db.calls/index';
+export * as 'db-returns' from './db.returns/index';
+export * as 'api-request-bodies' from './api.request.bodies/index';
+export * as 'api-querry-parameters' from './api.querry.parameters/index';
+export * as 'api-returns' from './api.returns/index';
