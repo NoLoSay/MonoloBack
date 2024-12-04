1143eeaed245e303aad7f480249285c86d0accf6
Aurèle Nicolas <aurele.nicolas@epitech.eu>
2024-12-04 22:35:19 +0100

    add tests to video service

--- libs/video.service/src/lib/video.service.spec.ts
+++ libs/video.service/src/lib/video.service.spec.ts
@@ -1,12 +1,15 @@
 import { HttpModule } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
-import { VideoService } from './video.service';
-import { PrismaBaseService } from '@noloback/prisma-client-base';
+import { getValidationStatusFromRole, VideoService } from './video.service';
+import { PrismaBaseService, Role, Video } from '@noloback/prisma-client-base';
 import { VideoCommonReturn } from '@noloback/api.returns';
 import { VideoCommonDbReturn } from '@noloback/db.returns';
 import { LoggerService } from '@noloback/logger-lib';
 import { ProfileService } from '@noloback/profile.service';
 import { SitesManagersService } from '@noloback/sites.managers.service';
+import { UserRequestModel } from '@noloback/requests.constructor';
+import { NotFoundException } from '@nestjs/common';
+import exp = require('constants');
 
 class Provider {
   constructor(url: string) {
@@ -34,6 +37,7 @@ class EmptyService {}
 
 describe('videoservice', () => {
   let app: TestingModule;
+  let prismaBase: PrismaBaseService;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
@@ -41,7 +45,19 @@ describe('videoservice', () => {
         VideoService,
         {
           provide: PrismaBaseService,
-          useClass: MockPrisma,
+          useValue: {
+            video: {
+              count: jest.fn(),
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+              findFirst: jest.fn(),
+              create: jest.fn(),
+              update: jest.fn(),
+            },
+            hostingProvider: {
+              findUnique: jest.fn(),
+            },
+          },
         },
         {
           provide: LoggerService,
@@ -58,6 +74,8 @@ describe('videoservice', () => {
       ],
       imports: [HttpModule],
     }).compile();
+
+    prismaBase = app.get<PrismaBaseService>(PrismaBaseService);
   });
 
   describe('getYoutube', () => {
@@ -66,12 +84,67 @@ describe('videoservice', () => {
       const video: VideoCommonReturn = new VideoCommonReturn(
         new VideoCommonDbReturn(),
       );
-      expect(await service.getYoutube(video)).toStrictEqual({
-        video: video,
-        url: 'aaa'
-          .replace('${videoUUID}', video.uuid)
-          .replace('${providerVideoId}', video.hostingProviderVideoId),
-      });
+      const findUniqueSpy = jest
+        .spyOn(prismaBase.hostingProvider, 'findUnique')
+        .mockResolvedValue({
+          url: 'aaa',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          deletedAt: null,
+          id: 1,
+          name: 'aaa',
+        });
+      expect((await service.getYoutube(video)).url).toStrictEqual('aaa');
+      expect(findUniqueSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('getValidationStatusFromRole', () => {
+    it('should return the validation status', async () => {
+      expect(await getValidationStatusFromRole(Role.ADMIN)).toStrictEqual([
+        'VALIDATED',
+        'PENDING',
+        'REFUSED',
+      ]);
+      expect(await getValidationStatusFromRole(Role.MODERATOR)).toStrictEqual([
+        'VALIDATED',
+        'PENDING',
+        'REFUSED',
+      ]);
+      expect(await getValidationStatusFromRole(Role.MANAGER)).toStrictEqual([
+        'VALIDATED',
+        'PENDING',
+      ]);
+      expect(await getValidationStatusFromRole(Role.USER)).toStrictEqual([
+        'VALIDATED',
+      ]);
+    });
+  });
+
+  describe('updateVideoShowcased', () => {
+    it('should throw if the video is not found', async () => {
+      const service = app.get(VideoService);
+      const video: VideoCommonReturn = new VideoCommonReturn(
+        new VideoCommonDbReturn(),
+      );
+      const user: UserRequestModel = {
+        id: 1234,
+        username: 'test',
+        email: 'test',
+        picture: 'test',
+        telNumber: 'test',
+        createdAt: new Date(),
+        activeProfile: { id: 1234, role: Role.MANAGER },
+        emailVerified: true,
+        password: 'test',
+      };
+      const findFirstSpy = jest
+        .spyOn(prismaBase.video, 'findFirst')
+        .mockResolvedValue(null);
+      expect(service.updateVideoShowcased(user, 1234, true)).rejects.toThrow(
+        NotFoundException,
+      );
+      expect(findFirstSpy).toHaveBeenCalled();
     });
   });
 });
